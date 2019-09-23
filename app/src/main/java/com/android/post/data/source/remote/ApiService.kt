016ae8d9c8cf03b49b78849bfa435d03b96db025
ZARA <heydari.z1990@gmail.com>
2019-09-23 11:54:33 +0330

    removed RX libs

--- app/src/main/java/com/android/post/data/source/remote/ApiService.kt
+++ app/src/main/java/com/android/post/data/source/remote/ApiService.kt
@@ -1,7 +1,6 @@
 package com.android.post.data.source.remote
 
 import com.android.post.data.model.Post
-import io.reactivex.Single
 import retrofit2.http.GET
 
 interface ApiService {
--- app/src/main/java/com/android/post/domain/repository/PostsRepository.kt
+++ app/src/main/java/com/android/post/domain/repository/PostsRepository.kt
@@ -1,7 +1,6 @@
 package com.android.post.domain.repository
 
 import com.android.post.data.model.Post
-import io.reactivex.Single
 
 interface PostsRepository {
 
