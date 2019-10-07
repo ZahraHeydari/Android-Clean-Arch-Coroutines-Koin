40696ce9055147e08c62d105ac2a84a27b898f5b
ZARA <heydari.z1990@gmail.com>
2019-10-07 15:14:58 +0330

    updated PostsRepositoryImp

--- app/src/main/java/com/android/post/data/repository/PostsRepositoryImp.kt
+++ app/src/main/java/com/android/post/data/repository/PostsRepositoryImp.kt
@@ -3,7 +3,6 @@ package com.android.post.data.repository
 import com.android.post.data.model.Post
 import com.android.post.data.source.remote.ApiService
 import com.android.post.domain.repository.PostsRepository
-import com.android.post.domain.usecase.base.UseCaseResponse
 
 class PostsRepositoryImp(private val apiService: ApiService) : PostsRepository {
 
